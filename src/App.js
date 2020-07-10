c11223a764d2b7cb7d4731e4c6081393cbdd2585
saber87 <saber87@naver.com>
2020-07-10 17:44:38 +0900

    Create Customer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>NDS Secret ingest development!!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id':'S9B5757',
+    'image': 'https://wise.sbs.co.kr/wise/GetPhoto.do?COCD=S&EMP_NO=9B5757',
+    'name': '이세영',
+    'birthday': '881205',
+    'gender': '여자',
+    'job': '기자'
+  },
+  {
+    'id':'S9B5484',
+    'image': 'https://wise.sbs.co.kr/wise/GetPhoto.do?COCD=S&EMP_NO=9B5484',
+    'name': '김병문',
+    'birthday': '870402',
+    'gender': '남자',
+    'job': '기술'
+  },
+  {
+  'id':'S9B3435',
+  'image': 'https://wise.sbs.co.kr/wise/GetPhoto.do?COCD=S&EMP_NO=9B3435',
+  'name': '장철',
+  'birthday': '881205',
+  'gender': '남자',
+  'job': '종편'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              /> ); }) }
+     </div>
+    );
+  }  
 }
 
 export default App;
