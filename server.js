36682d9093cac8e793dd08ba3e37545edf052bda
saber87 <saber87@naver.com>
2020-07-16 14:44:00 +0900

    Create Customer Database Table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id':'S9B5757',
-            'image': 'https://wise.sbs.co.kr/wise/GetPhoto.do?COCD=S&EMP_NO=9B5757',
-            'name': '이세영',
-            'birthday': '881205',
-            'gender': '여자',
-            'job': '기자'
-          },
-          {
-            'id':'S9B5484',
-            'image': 'https://wise.sbs.co.kr/wise/GetPhoto.do?COCD=S&EMP_NO=9B5484',
-            'name': '김병문',
-            'birthday': '870402',
-            'gender': '남자',
-            'job': '기술'
-          },
-          {
-          'id':'S9B3435',
-          'image': 'https://wise.sbs.co.kr/wise/GetPhoto.do?COCD=S&EMP_NO=9B3435',
-          'name': '장철',
-          'birthday': '881205',
-          'gender': '남자',
-          'job': '종편'
-          }
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+          res.send(rows);
+      }
+    )
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
